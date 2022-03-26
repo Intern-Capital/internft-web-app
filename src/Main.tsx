e9c75cc8585fdb83df6357f302bea157a9e096a8
J. Brandon Johnson <bjohnson@swizzmagik.com>
2022-03-26 08:38:16 -0700

    Deploy to prod

--- src/Main.tsx
+++ src/Main.tsx
@@ -4,11 +4,11 @@ import { Home } from "./components/pages/Home"
 import { Splash } from "./components/pages/Splash"
 
 export function Main() {
-	return (
-		<Routes>
-			<Route path='/' element={<Splash />} />
-			<Route path='/home' element={<Home />} />
-			<Route path='*' element={<Navigate replace to='/' />} />
-		</Routes>
-	)
+  return (
+    <Routes>
+      <Route path='/' element={<Splash />} />
+      {/* <Route path='/home' element={<Home />} /> */}
+      <Route path='*' element={<Navigate replace to='/' />} />
+    </Routes>
+  )
 }
